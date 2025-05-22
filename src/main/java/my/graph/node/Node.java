6a992adec2589fed5843559b0ecd5431d790902f
Hyonyoung Park <lugglory@gmail.com>
2025-05-22 18:35:00 +0900

    수정 중

--- src/main/java/my/graph/node/Node.java
+++ src/main/java/my/graph/node/Node.java
@@ -1,23 +1,40 @@
 package my.graph.node;
 
-import my.util.UnorderedList;
-
 import java.util.EnumMap;
 
-public class Node {
-    private int version;
+public class Node implements NodeView {
+    static NodeCleaner nodeCleaner;
+    static int count; // 논리적으로 살아있는 갯수
     private Data data;
-    private boolean isActive;
-    public final EnumMap<Direction, UnorderedList<Node>> nodes;
+    private boolean exist;
+    public final EnumMap<Direction, NodeViewList> adjacentNodes;
 
-    public Node() {
-        nodes = new EnumMap<>(Direction.class);
-        for (Direction dir : Direction.getDirections()) {
-            nodes.put(dir, new UnorderedList<>());
-        }
+    static {
+        nodeCleaner = new NodeCleaner();
     }
 
-    public int getVersion() {
-        return version;
+    public Node() {
+        exist = true;
+        adjacentNodes = new EnumMap<>(Direction.class);
+        for (Direction dir : Direction.getDirections()) {
+            adjacentNodes.put(dir, new NodeViewList());
+        }
+        count++;
+    }
+
+    public Node get() {
+        if (exist) {
+            return this;
+        }
+        return null;
+    }
+
+    public void delete() {
+        exist = false;
+        nodeCleaner.delete(this);
+    }
+
+    public void cleanNullViews(Direction direction) {
+        adjacentNodes.get(direction).cleanDeletedNodes();
     }
 }
